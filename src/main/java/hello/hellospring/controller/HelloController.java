975be697e780e3f07c8fe88254b4cdf168f33afd
jikkun <henjuck@gmail.com>
2021-04-28 17:05:53 +0900

    테스트 케이스 작성

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,45 @@
+package hello.hellospring.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String Hello(Model model){
+        model.addAttribute("data","abcd!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name", required = false) String name, Model model){
+        return "hello " + name; // hello {name}
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Getter
+    @Setter
+    static class Hello{
+        private String name;
+    }
+}
